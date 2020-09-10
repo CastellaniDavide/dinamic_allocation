3c32154403e0d3a0bc019520d323e347d7db4ac6
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-10 16:01:01 +0200

    Created bin/template.c

---
+++ bin/dinamic_allocation.c
@@ -0,0 +1,22 @@
+/**
+ * @file dinamic_allocation.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief Test an easy dinamic allocation in c
+ *
+ * @ingroup dinamic_allocation
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("dinamic_allocation");
+	return 0;
+}
